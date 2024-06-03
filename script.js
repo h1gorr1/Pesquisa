9a6da9cb8b4f402ad0630bd80fed5655cffee739
h1gorr1 <165603294+h1gorr1@users.noreply.github.com>
2024-06-03 00:30:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Criando uma instância do ScrollReveal e configurando o reset para true
+var revelar = ScrollReveal({ reset: true });
+
+// Aplicando a animação aos elementos com a classe '.corpo3 img'
+revelar.reveal('corpo3.img', {
+    duration: 3000,
+    distance: '90px'
+});
